c036ce13a9d856f70dbacfc9c4e3576b89bfba98
YourGitHubName <alan@wecancodeit.org>
2019-01-01 10:07:53 -0500

    initial load

---
+++ src/main/java/org/wecancodeit/ppmtool/PpmtoolApplication.java
@@ -0,0 +1,14 @@
+package org.wecancodeit.ppmtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpmtoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpmtoolApplication.class, args);
+	}
+
+}
+
